a4ffb30c83bf9ed08b64eda1bca76cefbb2f4e76
pionetwothree <75122491+pionetwothree@users.noreply.github.com>
2021-05-01 15:02:20 +0200

    Add files via upload

--- src/models/NoteSchema.js
+++ src/models/NoteSchema.js
@@ -1,14 +1,4 @@
 import mongoose from "mongoose";
 
-//export const Cat = mongoose.model("Cat", { name: String });
-//const mongoose = require("mongoose");
-export const notesGraphql = mongoose.model("Note", { name: String });
+export const notesGraphql = mongoose.model("Note", { name: String , content: String});
 
-//export const subscribeCats = mongoose.model("Note", { name: String });
-
-//const notesSchema = {
-//    title: String, 
-//    content: String
-//}
-
-//module.exports = notesSchema;
